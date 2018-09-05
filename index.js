c57cde3abaf8969936191af8dca1c0842c44f821
Hayo Friese <hayo.dtt@gmail.com>
2018-09-05 18:29:46 +0200

    Changed variable name to correlate to the one of Heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('path')
 const hostname = '127.0.0.1'
-const port = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => res.render('index'))
-	.listen(port, () => console.log(`Listening on ${hostname}:${ port }`))
+	.listen(PORT, () => console.log(`Listening on ${hostname}:${ PORT }`))
